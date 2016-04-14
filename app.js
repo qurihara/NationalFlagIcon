eefcbfc7332f08dc08d973c2457bf3adee366512
Kazutaka Kurihara <kurihara@kuriharakazutaka-no-MacBook.local>
2016-04-14 15:34:46 +0900

    ok

--- app.js
+++ app.js
@@ -22,12 +22,19 @@ server = http.createServer(function(req, res) {
     res.writeHead(200, {'content-type': 'text/html'});
     res.end(
       '<h4>The Unievrsal Background Filter for SNS Profile Picture</h4>'+
-      '<p>Upload your SNS profile picture and wait for a while.</p>'+
+      '<p>Upload your SNS profile picture and wait for a while (say, 30 sec. or so).</p>'+
       '<form action="/upload" enctype="multipart/form-data" method="post">'+
       '<input type="file" name="upload" multiple="multiple"><br>'+
       '<input type="submit" value="Upload">'+
       '</form>'+
-      ' <p>(c)Kazutaka Kurihara</p>'
+      '<p>Example:</p><table border="0"><tr>'+
+      '<td align="center"><img src="example/icon.jpg" width = "64" height = "64"></td>'+
+      '<td align="center">+</td>'+
+      '<td align="center"><img src="example/movie.gif" width = "64" height = "64"></td>'+
+      '<td align="center">=</td>'+
+      '<td align="center"><img src="example/resize_movie_icon.gif" width = "64" height = "64"></td>'+
+      '</tr></table>' +
+      '<p>(c)Kazutaka Kurihara</p>'
     );
   } else if (req.url == '/upload') {
     var form = new formidable.IncomingForm(),
@@ -105,68 +112,9 @@ server = http.createServer(function(req, res) {
               deleteFiles(pat,dir);
             }
           });
-
-          // filestr = filestr + "\t'" + files[i][1]['name'] + "',\n";
-          // fs.readFile(pat,function(err,data){
-          //   if(err){
-          //     return;
-          //   }
-          //   var str = data.toString('base64');
-          //   //do something
-          //
-          //   console.log(pat);
-          // });
         }
-        var data = ejs.render(template, {
-          imgs: filestr,
-          interval: 2000,
-          width: 1200,
-          height: 600,
-          firstview: 0,
-          rotation: true,
-          speed: -30
-        });
-        fs.writeFileSync("thetaview.html",data);
       });
     form.parse(req);
-  } else if (req.url.lastIndexOf('/view', 0) === 0){// .indexOf('/view/')>0) { // http://localhost:3000/view?cs=1419629829325&ce=1419629829325
-    console.log('view');
-    if(req.method=='GET') {
-      var url_parts = url.parse(req.url,true);
-      console.log(url_parts.query['cs'] + " - " + url_parts.query['ce']);
-      var _interval = url_parts.query['i'] || 30000;
-      var _width = url_parts.query['w'] || 1200;
-      var _height = url_parts.query['h'] || 600;
-      var _firstview = url_parts.query['f'] || 0;
-      var _rotation = url_parts.query['r'] || true;
-      var _speed = url_parts.query['sp'] || -15;
-      db.getEntryByCtime(Number(url_parts.query['cs']),Number(url_parts.query['ce']),function(result){
-        //console.log(result.length)
-        var b64s = '';
-        for(var i=0;i<result.length;i++){
-          b64s += "'data:image/jpg;base64," + result[i].mini_image + "',";
-        }
-        b64s = b64s.substring(0,b64s.length-1);
-        //console.log(b64s);
-        var data = ejs.render(template, {
-          imgs: b64s,
-          interval: _interval,
-          width: _width,
-          height: _height,
-          firstview: _firstview,
-          rotation: _rotation,
-          speed: _speed
-        });
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-
-        // console.log('time : ' + JSON.stringify(result.time));
-        // // console.log("image " + result.image); //**********
-        // socket.emit('update_image_base64',result.image);
-        // emitIosImage(socket, result.image);
-      });
-    }
   } else {
     fs.readFile(__dirname + req.url,function(err,data){
       if(err){
